de8a945ff9abe71a28d4b23a2db8fe3cdbdf505e
nina-jsl <jl13999@nyu.edu>
2025-04-04 16:59:04 -0400

    edits

--- web-app/app.py
+++ web-app/app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    """Route that returns a hello message."""
     return "Hello from Flask in Docker!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+
